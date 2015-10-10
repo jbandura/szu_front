a07b7ca60a350cd1b191fe3e6085deb0a54a9405
jbandura <j.bandura@borderstudio.eu>
2015-10-10 23:54:17 +0200

    move all actions to route and delete unnecessary component

--- app/pods/students/edit/route.js
+++ app/pods/students/edit/route.js
@@ -8,5 +8,19 @@ export default Route.extend({
       student: this.store.find('student', params.student_id),
       groups: this.store.findAll('student_group')
     });
+  },
+
+  actions: {
+    saveChanges(student) {
+      const flashMessages = Ember.get(this, 'flashMessages');
+      student.save().then((student) => {
+        this.transitionTo('students.show', student);
+        flashMessages.success('Student was successfully edited');
+      });
+    },
+    goBack() {
+      this.transitionTo('students.index');
+      return false;
+    }
   }
 });
