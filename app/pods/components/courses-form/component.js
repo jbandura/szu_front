d8869e8c2591cc94a0a628fbdd672663df692c66
jbandura <j.bandura@borderstudio.eu>
2015-10-11 17:43:26 +0200

    use rapid forms in courses/new + remove partial folder

--- app/pods/components/courses-form/component.js
+++ app/pods/components/courses-form/component.js
@@ -1,10 +1,30 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
-export default Component.extend({
+export default Component.extend(EmberValidations, {
   course: null, //passed in
   groups: null, //passed in
+  groupChosen: computed('course.studentGroup', function() {
+    return this.get('course.studentGroup');
+  }),
+  isButtonDisabled: computed('isValid', 'groupChosen', function() {
+    return !this.get('isValid') || !this.get('groupChosen');
+  }),
+
+  validations: {
+    "course.name": {
+      presence: true
+    },
+    "course.weekday": {
+      presence: true
+    },
+    "course.time": {
+      presence: true
+    }
+  },
+
   actions: {
     save: function() {
       console.log('saving...');
