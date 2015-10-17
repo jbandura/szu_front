dfb065a6e8ba74f001882ea611dd37c99b40b0d2
jbandura <j.bandura@borderstudio.eu>
2015-10-17 14:07:13 +0200

    add ability to delete presence list

--- app/pods/courses/presence-lists/index/route.js
+++ app/pods/courses/presence-lists/index/route.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('presence-list');
+  },
+
+  actions: {
+    deleteList(list) {
+      let flashMessages = Ember.get(this, 'flashMessages');
+      list.destroyRecord().then( () => {
+        flashMessages.success('Presence list was successfully deleted.');
+      });
+    }
   }
 });
